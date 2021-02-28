ebe307f782fa390b8967fae46f9991cb8e0648ae
kijube <kijubeofficial@gmail.com>
2021-02-28 05:21:57 -0500

    more work

--- imgpayload.py
+++ imgpayload.py
@@ -32,12 +32,18 @@ OUT_IMG_PLACEHOLDER = "{out}"
 
 MIN_BLOCK_SIZE = 3
 
+WORKER_COOLDOWN = 0
+
 MSG_TYPE_STATUS = "status"
 MSG_TYPE_RESULT = "result"
 MSG_TYPE_LOG = "log"
 MSG_STATUS_DONE = "done"
 
 JOB_TYPE_BLOCK_MATCHES = "job_block_matches"
+JOB_TYPE_CHECK_IMAGE = "job_check_image"
+JOB_TYPE_INJECT_PAYLOAD = "job_inject_payload"
+
+JOB_RESULT_CHECK_IMAGE_FAILED = "check_image_failed"
 
 
 def parse_arguments():
@@ -84,8 +90,8 @@ class ImgPayloader:
         self.beat_cmd = beat_cmd
         self.imgs = []
         self.global_task = None
-        self.thread_count = max(1, min(len(self.source_imgs), thread_count))
-        self.img_contexts = []
+        self.thread_count = max(1, thread_count)
+        self.img_contexts: list[ImageContext] = []
         self.workers: list[WorkerThread] = []
         self.job_queue = Queue()
 
@@ -109,7 +115,19 @@ class ImgPayloader:
             return False, out, errs
 
     def is_finished(self):
-        return False
+        if not self.job_queue.empty():
+            return False
+
+        for ctx in self.img_contexts:
+            job = ctx.get_new_job(modify=False)
+            if job is not None:
+                return False
+
+        for worker in self.workers:
+            if worker.is_working:
+                return False
+
+        return True
 
     def create_contexts(self):
         for img in self.imgs:
@@ -206,8 +224,10 @@ class ImageContext:
     def to_local_file(self, file):
         return join(self.dir, file)
 
-    def get_new_job(self):
+    def get_new_job(self, modify=True):
         if not self.jobs.empty():
+            if not modify:
+                return "still has jobs"
             return self.jobs.get()
         return None
 
@@ -218,16 +238,27 @@ class ImageContext:
         job_type = job_result.job_info.job_type
         if job_type == JOB_TYPE_BLOCK_MATCHES:
             self.block_matches = job_result.result
+            self.try_inject_payload()
+        elif job_type == JOB_TYPE_INJECT_PAYLOAD:
+            self.add_check_job(job_result)
+
+    def try_inject_payload(self):
+        for match in self.block_matches:
+            self.jobs.put(JobInfo(self, JOB_TYPE_INJECT_PAYLOAD, (match)))
+
+    def add_check_job(self, job_result):
+        img_file = job_result.result
+        self.jobs.put(JobInfo(self, JOB_TYPE_CHECK_IMAGE, (img_file)))
 
 
 class JobInfo:
     def __init__(self, img_context: ImageContext, job_type, job_data):
         self.img_context = img_context
         self.job_type = job_type
-        self.job_data = job_data
+        self.job_args = job_data
 
     def __str__(self):
-        return f"Job '{self.job_type}' for image '{self.img_context.name}' with args '{str(self.job_data)[:100]}'"
+        return f"Job '{self.job_type}' for image '{self.img_context.name}' with args '{str(self.job_args)[:100]}'"
 
 
 class WorkerContext:
@@ -244,6 +275,12 @@ class WorkerThread(threading.Thread):
         self.messages = Queue()
         self.running = True
         self.is_working = False
+        self.cooldown = 0
+        self.job_handlers = {
+            JOB_TYPE_BLOCK_MATCHES: self.compare_blocks,
+            JOB_TYPE_INJECT_PAYLOAD: self.inject_payload,
+            JOB_TYPE_CHECK_IMAGE: self.check_image
+        }
 
     def send_message(self, type_: str, value):
         self.messages.put((type_, value))
@@ -256,15 +293,15 @@ class WorkerThread(threading.Thread):
             return False
 
     def work(self):
+        self.cooldown = WORKER_COOLDOWN
         self.log("got new job " + str(self.job_info))
-        if self.job_info.job_type == JOB_TYPE_BLOCK_MATCHES:
-            self.compare_blocks()
+        handler = self.job_handlers[self.job_info.job_type]
+        handler(self.job_info, self.job_info.job_args)
 
     def send_result(self, result):
         self.send_message(MSG_TYPE_RESULT, JobResult(self, self.job_info, result))
 
     def done(self):
-        self.is_working = False
         self.send_message(MSG_TYPE_STATUS, MSG_STATUS_DONE)
 
     def log(self, *msg):
@@ -283,10 +320,38 @@ class WorkerThread(threading.Thread):
                 self.is_working = True
                 self.work()
                 self.done()
+            else:
+                self.cooldown -= 1
+                if self.cooldown < 0:
+                    self.is_working = False
 
-    def compare_blocks(self):
-        original = self.job_info.img_context.create_tmp_copy()
-        to_process = self.job_info.img_context.create_tmp_copy()
+    def inject_payload(self, job_info: JobInfo, args):
+        match: BlockMatch = args
+        img = self.job_info.img_context.create_tmp_copy()
+        data = read(img)
+        payload = bytes(self.payloader.payload, "ascii")  # todo make encoding changeable?
+        edited_data = b""
+        edited_data += bytes(data[:match.index])
+        edited_data += bytes(payload)
+        edited_data += bytes(data[match.index + len(payload):])
+
+        self.send_result(img)
+
+    def check_image(self, job_info: JobInfo, args):
+        img_file = args
+        result_file = img_file + "_prcd"
+        self.payloader.run_beat_cmd(img_file, result_file)
+        if not exists(result_file):
+            self.send_result((JOB_RESULT_CHECK_IMAGE_FAILED,))
+            return
+
+        data = read(result_file)
+        if bytes(self.payloader.payload, "ascii") in data:# todo make encoding changeable?
+            print("FUCK YEAH")
+
+    def compare_blocks(self, job_info: JobInfo, args):
+        original = job_info.img_context.create_tmp_copy()
+        to_process = job_info.img_context.create_tmp_copy()
         processed = to_process + "_prcd"
         success = self.payloader.run_beat_cmd(to_process, processed)
         if not success:
@@ -330,6 +395,15 @@ class JobResult:
         self.worker = worker
         self.job_info = job_info
         self.result = result
+
+
+class BlockMatch:
+    def __init__(self, index):
+        self.index = index
+        self.size = 0
+
+    def __str__(self):
+        return f"{self.size} @ {self.index}"
 
 
 class ImageProcessorThread(threading.Thread):
@@ -423,15 +497,6 @@ class ImageProcessorThread(threading.Thread):
         # todo check if payload fits inside?
 
         self.log(f"Found {len(self.block_matches)} block matches, trying to fit payload...")
-
-
-class BlockMatch:
-    def __init__(self, index):
-        self.index = index
-        self.size = 0
-
-    def __str__(self):
-        return f"{self.size} @ {self.index}"
 
 
 if __name__ == "__main__":
